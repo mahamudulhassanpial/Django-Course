9a7df7317c07e9401ec98833173c4a634cb2b053
Mahamudul Hassan <mahamudulhassan5678@gmail.com>
2024-10-18 16:31:56 +0600

    django templating code

---
+++ project_3/first_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import datetime
+
+# Create your views here.
+def home(request):
+    d = {'author' : 'Rahim', 'age' : 20, 'list' : ['python','is','best'], 
+         'birthday' : datetime.datetime.now(), 'val' : ' ','courses' : [
+
+        {
+            'id' : 1,
+            'name' : 'Python',
+            'fee' : 5000,
+        },
+        {
+            'id' : 2,
+            'name' : 'Django',
+            'fee' : 10000,
+        },
+        {
+            'id' : 3,
+            'name' : 'C',
+            'fee' : 1000,
+        },
+    ]}
+    # return render(request, 'home.html',context=d)
+    return render(request, 'home.html', d)
